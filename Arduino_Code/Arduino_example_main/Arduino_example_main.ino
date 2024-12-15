c3be6f454a9853a3acfa505373bce695cad1a07d
Jordon-Notts <efyja7@nottingham.ac.uk>
2024-12-15 21:54:11 +0000

    archived out old arduino code

---
+++ Arduino_Code/Arduino_example_main/Arduino_example_main.ino
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include "CommHandler.h"
+
+#define CS_PIN    2
+#define CLOCK_PIN 3
+#define DATA_PIN  4
+#define LED_PIN   13
+
+// Example callback function
+void exampleCallback(String data) {
+
+  Serial.print("Received: ");
+
+  Serial.println(data);
+  
+}
+
+// Instantiate CommHandler object
+CommHandler comm(CS_PIN, CLOCK_PIN, DATA_PIN, LED_PIN, exampleCallback);
+
+void setup() {
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  String working_string = "12345";
+
+  comm.send_string(working_string);
+
+  Serial.println(working_string);
+
+  delay(1000);
+}
